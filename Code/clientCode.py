a7cb1eee67e47a65b73552667376f6e57ed1e43b
Danis Fermi <danisfermijohn@gmail.com>
2016-11-17 23:13:22 -0500

    Added client Code sample

---
+++ Code/clientCode.py
@@ -0,0 +1,9 @@
+from socket import *
+serverName =  'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input("Input lowercase sentence:")
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage.decode()
+clientSocket.close()
