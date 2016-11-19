e30e5da587542acb80ade77c94143cc17f28ed4a
Danis Fermi <danisfermijohn@gmail.com>
2016-11-19 02:27:54 -0500

    Added connection try to the client code

--- code/clientCode.py
+++ code/clientCode.py
@@ -1,16 +1,22 @@
-import library
+from socket import *
 
-"""
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input("Input lowercase sentence:")
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage.decode()
-clientSocket.close()
-"""
+serverList = ["127.0.0.1","192.168.0.102"] # 2 server IP's to be added here
+serverPort = [i for i in xrange(20000,20009)]
 
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.bind(('', 7733))
+connectFlag=True
+
+for i in serverList:
+    for j in serverPort:
+        if clientSocket.connect(i,j) is True:
+            print "You are now connected to Server " + i + " on Port Number" + j
+            break
+        else:
+            connectFlag=False
+
+if connectFlag is False:
+    print "Connection Error. Please try again later"
 
 class Client(object):
   def __init__(self):
