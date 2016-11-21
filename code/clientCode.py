11bca7c4d59fb9a53bc4c89336ee1ad8e9b0d0d0
Danis Fermi <danisfermijohn@gmail.com>
2016-11-20 21:25:41 -0500

    Modified cient code.

--- code/clientCode.py
+++ code/clientCode.py
@@ -1,23 +1,29 @@
 from socket import *
 
-serverList = ["127.0.0.1","192.168.0.102"] # 2 server IP's to be added here
-serverPort = [i for i in xrange(20000,20009)]
+serverList = ["127.0.0.1", "192.168.0.102"]  # 2 server IP's to be added here
+serverPort = [i for i in xrange(20000, 20009)]
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.bind(('', 7733))
-connectFlag=True
+connectFlag = True
 
 for i in serverList:
     for j in serverPort:
-        if clientSocket.connect(i,j) is True:
+        if clientSocket.connect(i, j) is True:
             print "You are now connected to Server " + i + " on Port Number" + j
             break
         else:
-            connectFlag=False
+            connectFlag = False
 
 if connectFlag is False:
     print "Connection Error. Please try again later"
 
 class Client(object):
-  def __init__(self):
-    print "Hello World"
+    info=[]
+    def __init__(self):
+        self.info.append(raw_input("Enter the username you wish to be identified as:"))
+        self.info.append("")  # Delimited to be added here
+
+    def send(self):
+        clientSocket.sendto(self.info.encode())
+
