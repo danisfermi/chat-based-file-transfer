90c81682f1a4e6bc55ea9b441e445ed51589e00e
aaugust4 <arjunaugustine@gmail.com>
2016-11-21 23:48:09 -0500

    even more

--- code/serverCode.py
+++ code/serverCode.py
@@ -8,8 +8,11 @@ class Server(object):
 
   def __init__(self):
     self.s = socket(AF_INET, SOCK_STREAM)
+    self.listen_port = 0
     self.clients = []
     self.chatrooms = []
+    self.client_count = 0
+    self.room_count = 0
 
   class ChatRoom(object):
     """
@@ -18,24 +21,34 @@ class Server(object):
 
     def __init__(self, server_reference, name, client_id):
       """
-
-      :param server_reference:
-      :param name:
-      :param id:
-      :param client:
+      ChatRoom ID is auto generated from server's room count history.
+      :param server_reference: Pointer to server object that instantiated this class
+      :param name: Chatroom name
+      :param client_id: ID of client that created the room, to be added to list of clients.
       """
       self.server = server_reference
       self.name = name
-      self.id = len(server_reference.chatrooms)
+      self.id = server_reference.room_count
       self.clients = [client_id]
 
     def broadcast(self, msg, source=None):
+      """
+      Function to broadcast a message from some source to all other clients in chatroom
+      :param msg: Raw message to be sent
+      :param source: Add a from source field to message and send to all other clients
+      """
+      flag = False
       if source:
         msg = 'FROM:' + source + '|' + msg
       for i in xrange(len(self.clients)):
         client_id = self.clients[i].client_id
-        socket = self.server.clients[client_id].socket
-        send_data(socket, msg)
+        clients = self.server.clients
+        if clients[client_id].name is not source:
+          send_data(clients[client_id].socket, msg)
+        else:
+          flag = True
+      if not flag:
+        sys.exit('Source client not in chatroom client list')
 
   class ClientNode(object):
     """
@@ -45,10 +58,11 @@ class Server(object):
     def __init__(self, server_reference, ip, socket, client_id):
       """
 
-      :param ip:
-      :param socket:
+      :param ip: IP address of client.
+      :param socket: Socket at server used to communicate with client.
       :param client_id:
       """
+      # TODO: Client ID cant be used to index clients array if previous clients have exitted.
       self.server = server_reference
       self.ip = ip
       self.socket = socket
@@ -57,16 +71,26 @@ class Server(object):
       self.suspended = False
       self.chatroom_admin = False
 
+    def execute(self):
+      """
+      This is the main per-client execution thread for the server.
+      Enable a client to login and proceed to listen to, and forward, its messages.
+      """
+      self.accept_login()
+      while not self.suspended:
+      # TODO
+      return False
+
     def accept_login(self):
+      """
+      Establish the clients username and create a new chatroom, or add it to an existing one.
+      """
       self.check_username()
       self.check_chatroom()
 
     def check_username(self, tries=5):
       """
       Try tries number of times to get a unique username from client.
-      :param client_id:
-      :param tries:
-      :return: login message from client
       """
       msg = decode_data(recv_data(self.socket))
       name = msg[0]
@@ -84,6 +108,9 @@ class Server(object):
       return
 
     def check_chatroom(self, tries=5):
+      """
+      Check if client wants to create a new chatroom or join an existing one.
+      """
       if self.suspended
         return
       msg = decode_data(recv_data(self.socket))
@@ -104,7 +131,10 @@ class Server(object):
           self.suspended = True
 
     def create_chatroom(self, tries=5):
-      # Create chatroom, send OK to client. Inform other clients/servers.
+      """
+      Create chatroom - Add client as first user of room when a unique name is received
+      :param tries: Retry 'tries' number of times to get a unique chatroom name.
+      """
       if self.suspended:
         return
       msg = decode_data(recv_data(self.socket))
@@ -123,6 +153,10 @@ class Server(object):
 
 
     def join_chatroom(self, tries=5):
+      """
+      Join chatroom - Add client to client list of room when request is received
+      :param tries: Retry 'tries' number of times to get an existent chatroom name.
+      """
       if self.suspended
         return
       msg = decode_data(recv_data(self.socket))
@@ -141,19 +175,16 @@ class Server(object):
         send_err(self.socket, 'Sorry, max tries exceeded. Aborting.')
         self.suspended = True
 
-    def execute(self):
-      self.accept_login()
-      while not self.suspended:
-      # TODO
-      return False
-
 
   # Server class functions follow
 
-  def go_online(self, tries=10, start=20000):
+  def go_online(self, start=20000, tries=10):
+    """
+    Bind to a connection port and start listening on it.
+    :param start: Try binding to port numbers starting at this value
+    :param tries: Try binding 'tries' number of times till port = start+tries-1
+    """
     flag = False
-
-    # Bind to a connection port from start to start+tries-1.
     for listen_port in xrange(start, start+tries-1):
       if bind_to_port(self.s, listen_port):
         flag = True
@@ -166,13 +197,18 @@ class Server(object):
     self.s.listen(25)
     print 'Server is listening at', listen_port
     # data_port = library.bind_to_random(tries)  # bind to a random port for data
-    return listen_port  # , data_port
+    self.listen_port = listen_port
 
   def execute(self):
+    """
+    This is the main server thread. Go online and start listening for connections.
+    Accept client connections and start a new client thread using its execute method.
+    """
     self.go_online()
     while True:
       ds, addr = self.s.accept()
-      client = ClientNode(self, addr, ds, len(self.clients))
+      client = ClientNode(self, addr, ds, self.client_count)
+      self.client_count += 1
       self.clients.append(client)
       print 'Incoming connection from', addr
       thread.start_new_thread(client.execute())
